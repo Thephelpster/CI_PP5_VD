5843a95d066aa026bb1e2fd513be7ad281813d01
jamie <thephelpster1992@gmail.com>
2024-01-12 14:21:49 +0000

    add source files predictive analysis and data management to show ml performance page

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from app_pages.multipage import MultiPage
 # loads page scripts
 from app_pages.page_summary import page_summary_body
 from app_pages.page_vehicle_visualiser import page_vehicle_visualiser_body
-#from app_pages.page_ml_performance import page_ml_performance_metrics
+from app_pages.page_ml_performance import page_ml_performance_metrics
 #from app_pages.page_vehicle_detector import page_vehicle_detector_body
 #from app_pages.page_project_hypothesis import page_project_hypothesis_body
 
@@ -15,7 +15,7 @@ app = MultiPage(app_name='Vehicle Detection App')
 # Add app pages here using .add_page()
 app.add_page('Project Summary', page_summary_body)
 app.add_page('Vehicle Visualiser', page_vehicle_visualiser_body)
-#app.add_page('Model Performance', page_ml_performance_metrics)
+app.add_page('Model Performance', page_ml_performance_metrics)
 #app.add_page('Vehicle Detection', page_vehicle_detector_body)
 #app.add_page('Project Hypothesis', page_project_hypothesis_body)
 app.run()  # Run the app
--- app_pages/page_ml_performance.py
+++ app_pages/page_ml_performance.py
@@ -11,19 +11,19 @@ def page_ml_performance_metrics():
     st.write("### Train, Validation and Test Set: Labels Frequencies")
 
     labels_distribution = plt.imread(
-        f"outputs/{version}/labels_distribution.png")
+        f"outputs/{version}/sets_distribution_pie.png")
     st.image(labels_distribution,
              caption='Labels Distribution on Train, Validation and Test Sets')
     st.write("---")
 
     st.write("### Model History")
-    col1, col2 = st.columns(2)
-    with col1:
-        model_acc = plt.imread(f"outputs/{version}/model_training_acc.png")
-        st.image(model_acc, caption='Model Training Accuracy')
-    with col2:
-        model_loss = plt.imread(f"outputs/{version}/model_training_losses.png")
-        st.image(model_loss, caption='Model Training Losses')
+
+    model_acc = plt.imread(
+        f"outputs/{version}/model_training_acc.png")
+    st.image(model_acc, caption='Model Training Accuracy')
+
+    model_loss = plt.imread(f"outputs/{version}/model_training_losses.png")
+    st.image(model_loss, caption='Model Training Losses')
     st.write("---")
 
     st.write("### Generalised Performance on Test Set")
@@ -32,8 +32,6 @@ def page_ml_performance_metrics():
     st.write("---")
 
     st.write("### Confusion Matrix")
-    col1 = st.columns(1)[0]
-    with col1:
-        model_acc = plt.imread(f"outputs/{version}/confusion_matrix.png")
-        st.image(model_acc, caption='Confusion Matrix')
+    model_acc = plt.imread(f"outputs/{version}/confusion_matrix.png")
+    st.image(model_acc, caption='Confusion Matrix')
     st.write("---")
