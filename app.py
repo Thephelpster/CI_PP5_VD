7a35af8387e310b51e21d514699b67305cc26ef4
jamie <thephelpster1992@gmail.com>
2024-01-12 14:46:25 +0000

    add vehicle detector page and input for new images

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from app_pages.multipage import MultiPage
 from app_pages.page_summary import page_summary_body
 from app_pages.page_vehicle_visualiser import page_vehicle_visualiser_body
 from app_pages.page_ml_performance import page_ml_performance_metrics
-#from app_pages.page_vehicle_detector import page_vehicle_detector_body
+from app_pages.page_vehicle_detector import page_vehicle_detector_body
 #from app_pages.page_project_hypothesis import page_project_hypothesis_body
 
 
@@ -16,6 +16,6 @@ app = MultiPage(app_name='Vehicle Detection App')
 app.add_page('Project Summary', page_summary_body)
 app.add_page('Vehicle Visualiser', page_vehicle_visualiser_body)
 app.add_page('Model Performance', page_ml_performance_metrics)
-#app.add_page('Vehicle Detection', page_vehicle_detector_body)
+app.add_page('Vehicle Detection', page_vehicle_detector_body)
 #app.add_page('Project Hypothesis', page_project_hypothesis_body)
 app.run()  # Run the app
