a15b382a81bf2708f0e89e1bc29dbe831289b5ea
jamie <thephelpster1992@gmail.com>
2024-01-12 10:44:36 +0000

    add to project summary page

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from app_pages.multipage import MultiPage
 
 # loads page scripts
 from app_pages.page_summary import page_summary_body
-from app_pages.page_vehicle_visualizer import page_vehicle_body
-from app_pages.page_ml_performance import page_ml_performance_metrics
-from app_pages.page_vehicle_detector import page_vehicle_detector_body
-from app_pages.page_project_hypothesis import page_project_hypothesis_body
+#from app_pages.page_vehicle_visualizer import page_vehicle_body
+#from app_pages.page_ml_performance import page_ml_performance_metrics
+#from app_pages.page_vehicle_detector import page_vehicle_detector_body
+#from app_pages.page_project_hypothesis import page_project_hypothesis_body
 
 
 # Creates an instance of the app
@@ -14,8 +14,8 @@ app = MultiPage(app_name='Vehicle Detection App')
 
 # Add app pages here using .add_page()
 app.add_page('Project Summary', page_summary_body)
-app.add_page('Vehicle Visualizer', page_vehicle_visualizer_body)
-app.add_page('Model Performance', page_ml_performance_metrics)
-app.add_page('Vehicle Detection', page_vehicle_detector_body)
-app.add_page('Project Hypothesis', page_project_hypothesis_body)
+#app.add_page('Vehicle Visualizer', page_vehicle_visualizer_body)
+#app.add_page('Model Performance', page_ml_performance_metrics)
+#app.add_page('Vehicle Detection', page_vehicle_detector_body)
+#app.add_page('Project Hypothesis', page_project_hypothesis_body)
 app.run()  # Run the app
